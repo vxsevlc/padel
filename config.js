3e99d82a3e721f022cfd89f737618b7d98495732
vxse <vicoexport@gmail.com>
2025-08-31 20:00:18 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+window.SUPABASE_URL  = "https://pkcjqdjrukjdoznnqvvz.supabase.co";
+window.SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrY2pxZGpydWtqZG96bm5xdnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NTk2MzYsImV4cCI6MjA3MjIzNTYzNn0.QuGIza3pYVvjvhNvdXeUNTeltuTFWFQkzN12FFj__Kw";
